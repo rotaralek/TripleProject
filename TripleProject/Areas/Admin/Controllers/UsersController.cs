bce7022e962db45c42e135815cb04699d95e35bc
Alexei Rotari <rotaralek@gmail.com>
2019-05-17 09:50:07 +0300

    Added Users CRUD

--- TripleProject/Areas/Admin/Controllers/UsersController.cs
+++ TripleProject/Areas/Admin/Controllers/UsersController.cs
@@ -22,9 +22,131 @@ namespace TripleProject.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
+        //Get: Admin/Users
         public async Task<IActionResult> Index()
         {
             return View(await _userManager.Users.ToListAsync());
+        }
+        
+        // GET: Admin/Users/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Admin/Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserName,Email,PhoneNumber")] IdentityUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userManager.CreateAsync(user);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
+        }
+
+        // GET: Admin/Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Admin/Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,Email,PhoneNumber")] IdentityUser user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var User = await _userManager.FindByIdAsync(user.Id);
+                    User.UserName = user.UserName;
+                    var result = await _userManager.UpdateAsync(User);
+
+                    if (!result.Succeeded)
+                    {
+                        ModelState.AddModelError("", result.Errors.First().ToString());
+                        return View();
+                    }
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return View(user);
+        }
+
+        // GET: Admin/Users/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Admin/Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            await _userManager.DeleteAsync(user);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
