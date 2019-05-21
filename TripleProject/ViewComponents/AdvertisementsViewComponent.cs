4642547a6ced21c38f32b98bd9a52bdefcf9f4ba
Alexei Rotari <rotaralek@gmail.com>
2019-05-21 14:53:25 +0300

    Added owl slider Added recent advertisements and products on home page

---
+++ TripleProject/ViewComponents/AdvertisementsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TripleProject.Areas.Admin.Models;
+using TripleProject.Data;
+
+namespace TripleProject.ViewComponents
+{
+    public class AdvertisementsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdvertisementsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            IEnumerable<Advertisement> advertisements = _context.Advertisements.Include(p => p.Image).OrderByDescending(p => p.Id).Take(5);
+
+            return View("Default", advertisements);
+        }
+    }
+}
