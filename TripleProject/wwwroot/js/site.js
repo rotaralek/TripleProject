88a747db7b32fcaa9d214f2b859553da8228cda1
Alexei Rotari <rotaralek@gmail.com>
2019-05-21 15:19:02 +0300

    Fixed owl nav

--- TripleProject/wwwroot/js/site.js
+++ TripleProject/wwwroot/js/site.js
@@ -87,19 +87,16 @@
         $(this).owlCarousel({
             loop: true,
             margin: 15,
-            nav: true,
+            nav: false,
             responsive: {
                 0: {
-                    items: 1,
-                    nav: true
+                    items: 1
                 },
                 600: {
-                    items: 3,
-                    nav: false
+                    items: 3
                 },
                 1000: {
-                    items: 3,
-                    nav: true
+                    items: 3
                 }
             }
         });
