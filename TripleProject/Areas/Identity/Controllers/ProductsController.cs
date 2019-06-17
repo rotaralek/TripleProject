4aca68db7893c086c63e9c6664e14565fcd5eadf
Alexei Rotari <rotaralek@gmail.com>
2019-06-17 11:30:21 +0300

    Added image to the adv, prod user account manager

--- TripleProject/Areas/Identity/Controllers/ProductsController.cs
+++ TripleProject/Areas/Identity/Controllers/ProductsController.cs
@@ -32,7 +32,7 @@ namespace TripleProject.Areas.Identity.Controllers
             int itemsPerPage = 10;
             int skip = itemsPerPage * (page - 1);
             int count = await _context.Products.Where(u => u.UserId == userId).CountAsync();
-            var applicationDbContext = await _context.Products.Where(u => u.UserId == userId).OrderByDescending(p => p.DateTime).Skip(skip).Take(itemsPerPage).ToListAsync();
+            var applicationDbContext = await _context.Products.Where(u => u.UserId == userId).Include(p => p.Image).OrderByDescending(p => p.DateTime).Skip(skip).Take(itemsPerPage).ToListAsync();
 
             ViewData["count"] = count;
             ViewData["page"] = page;
