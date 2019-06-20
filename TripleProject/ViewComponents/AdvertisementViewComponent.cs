c428f1d5f2252b1edac731064a965e8cf52330c1
Alexei Rotari <rotaralek@gmail.com>
2019-06-20 16:00:08 +0300

    Created common product and advertisement components

---
+++ TripleProject/ViewComponents/AdvertisementViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TripleProject.Areas.Admin.Models;
+
+namespace TripleProject.ViewComponents
+{
+    public class AdvertisementViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Advertisement advertisement)
+        {
+            return View("Default", advertisement);
+        }
+    }
+}
---
+++ TripleProject/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TripleProject.Areas.Admin.Models;
+
+namespace TripleProject.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Product product)
+        {
+            return View("Default", product);
+        }
+    }
+}
